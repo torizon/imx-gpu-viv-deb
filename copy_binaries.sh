edc6142c526d03d39cddbb49231ce88dc6058485
Eugene Nuribekov <eugene.nuribekov@toradex.com>
2019-10-01 16:32:56 +0300

    Add -dev package creation
    control and install file updated to create package with pkg-config and
    header files

--- copy_binaries.sh
+++ copy_binaries.sh
@@ -1,11 +1,11 @@
 #! /usr/bin/env bash
 
-#set -x
+set +x
 
 files=$(cat src.list)
 mkdir -p src
 for f in $files
 do
-	cp -d ../gpu-core/usr/lib/$f src
+	find ../gpu-core -name "$f" -exec cp -d {} src ';'
 done
 
