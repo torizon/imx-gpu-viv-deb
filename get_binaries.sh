6c89d3e28e398fc606de2cbc78aa74563ffa1dfc
Abdur Rehman <abdur.rehman@toradex.com>
2020-07-08 16:23:12 +0500

    add checksum verification
    Related-to: TOR-1098
    
    Signed-off-by: Abdur Rehman <abdur.rehman@toradex.com>

--- get_binaries.sh
+++ get_binaries.sh
@@ -3,6 +3,7 @@ set +x
 URL=https://www.nxp.com/lgfiles/NMG/MAD/YOCTO/
 FILENAME=imx-gpu-viv
 FILEVER=$(cat binary.version)
+CHKSUM=$(cat binary.md5sum)
 FILEARCH=aarch64
 FULLNAME=$FILENAME-$FILEVER-$FILEARCH
 ARCHNAME=$FULLNAME.bin
@@ -10,6 +11,12 @@ ARCHNAME=$FULLNAME.bin
 if [ ! -f $ARCHNAME ]
 then
 	wget $URL/$ARCHNAME && chmod +x $ARCHNAME
+fi
+
+md5sum -c - <<< "$CHKSUM  $ARCHNAME"
+if [ "$?" != "0" ]; then
+	echo "Error: Checksum verification failed. Aborting!"
+	exit 1
 fi
 
 ./$ARCHNAME --auto-accept
